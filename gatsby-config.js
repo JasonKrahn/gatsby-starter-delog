dd483378340181787db7efe62397280097a3fad9
JasonKrahn <64810689+JasonKrahn@users.noreply.github.com>
2020-10-08 09:32:51 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,8 +67,6 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-        stylesPath: `${__dirname}/src/cms/admin.css`,
         enableIdentityWidget: true
       }
     },
